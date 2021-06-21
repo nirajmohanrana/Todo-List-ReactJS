128d485d0af37ee174779b0d64d964fa1d0b4a3d
nirajmohanrana <nirajmohanrana@gmail.com>
2021-06-21 11:18:50 +0530

    Completed Todo List

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an About Component
+            <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita fuga nostrum eius quaerat vero recusandae mollitia maiores delectus minus rerum, nihil ipsam voluptates nemo est, alias quasi officia odio consectetur culpa unde nulla sunt? </p>
+        </div>
+    )
+}
---
+++ src/MyComponents/Todos.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {TodoItem} from "./TodoItem";
+
+export const Todos = (props) => {
+    let myStyle = {
+        minHeight: "70vh",
+        margin: "40px auto"
+    }
+
+    return (
+        <div className="conatainer my-3 mx-5" style={myStyle}>
+            <h3 className="text-center my-3">Todos List</h3>
+            {props.todos.length===0? "No Todos to Display": 
+                props.todos.map((todo)=>{
+                    return  (
+                            <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/>
+                    )
+                })
+            }
+            
+        </div>
+    )
+}
